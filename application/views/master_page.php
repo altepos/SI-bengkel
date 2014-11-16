85f46412b4591dcdb2b8e11078b24e3315725e3c
haqisaurus <haqisaurus@gmail.com>
2014-11-16 12:28:45 +0700

    + add offline dependency

--- application/views/master_page.php
+++ application/views/master_page.php
@@ -4,10 +4,10 @@
         <meta charset="UTF-8">
         <title>AdminLTE | Dashboard</title>
         <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
-        <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-        <link href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+        <link href="<?php echo base_url('assets/css/bootstrap/bootstrap.min.css') ?>" rel="stylesheet" type="text/css" />
+        <link href="<?php echo base_url('assets/css/font-awesome/font-awesome.min.css') ?>" rel="stylesheet" type="text/css" />
         <!-- Ionicons -->
-        <link href="//code.ionicframework.com/ionicons/1.5.2/css/ionicons.min.css" rel="stylesheet" type="text/css" />
+        <link href="<?php echo base_url('assets/css/ionicons/ionicons.min.css') ?>" rel="stylesheet" type="text/css" />
         <!-- Morris chart -->
         <link href="<?php echo base_url('assets/css/morris/morris.css') ?>" rel="stylesheet" type="text/css" />
         <!-- jvectormap -->
@@ -21,7 +21,29 @@
         <!-- Theme style -->
         <link href="<?php echo base_url('assets/css/AdminLTE.css') ?>" rel="stylesheet" type="text/css" />
 
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+        <script src="<?php echo base_url('assets/js/plugins/jquery/jquery-2.1.1.min.js') ?>"></script>
+        <!-- add new calendar event modal -->
+
+        <script src="<?php echo base_url('assets/js/plugins/bootstrap/bootstrap.min.js') ?>" type="text/javascript"></script>
+        <script src="<?php echo base_url('assets/js/plugins/jquery-ui/jquery-ui.min.js') ?>" type="text/javascript"></script>
+        <!-- Morris.js charts -->
+        <script src="<?php echo base_url('assets/js/plugins/raphael/raphael-min.js') ?>"></script>
+        <script src="<?php echo base_url('assets/js/plugins/morris/morris.min.js') ?>" type="text/javascript"></script>
+        <!-- Sparkline -->
+        <script src="<?php echo base_url('assets/js/plugins/sparkline/jquery.sparkline.min.js') ?>" type="text/javascript"></script>
+        <!-- jvectormap -->
+        <script src="<?php echo base_url('assets/js/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js') ?>" type="text/javascript"></script>
+        <script src="<?php echo base_url('assets/js/plugins/jvectormap/jquery-jvectormap-world-mill-en.js') ?>" type="text/javascript"></script>
+        <!-- jQuery Knob Chart -->
+        <script src="<?php echo base_url('assets/js/plugins/jqueryKnob/jquery.knob.js') ?>" type="text/javascript"></script>
+        <!-- daterangepicker -->
+        <script src="<?php echo base_url('assets/js/plugins/daterangepicker/daterangepicker.js') ?>" type="text/javascript"></script>
+        <!-- datepicker -->
+        <script src="<?php echo base_url('assets/js/plugins/datepicker/bootstrap-datepicker.js') ?>" type="text/javascript"></script>
+        <!-- Bootstrap WYSIHTML5 -->
+        <script src="<?php echo base_url('assets/js/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js') ?>" type="text/javascript"></script>
+        <!-- iCheck -->
+        <script src="<?php echo base_url('assets/js/plugins/iCheck/icheck.min.js') ?>" type="text/javascript"></script>
 
 
         <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
@@ -405,28 +427,6 @@
         </div><!-- ./wrapper -->
 
         <!-- add new calendar event modal -->
-
-
-        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js" type="text/javascript"></script>
-        <script src="//code.jquery.com/ui/1.11.1/jquery-ui.min.js" type="text/javascript"></script>
-        <!-- Morris.js charts -->
-        <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-        <script src="<?php echo base_url('assets/js/plugins/morris/morris.min.js') ?>" type="text/javascript"></script>
-        <!-- Sparkline -->
-        <script src="<?php echo base_url('assets/js/plugins/sparkline/jquery.sparkline.min.js') ?>" type="text/javascript"></script>
-        <!-- jvectormap -->
-        <script src="<?php echo base_url('assets/js/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js') ?>" type="text/javascript"></script>
-        <script src="<?php echo base_url('assets/js/plugins/jvectormap/jquery-jvectormap-world-mill-en.js') ?>" type="text/javascript"></script>
-        <!-- jQuery Knob Chart -->
-        <script src="<?php echo base_url('assets/js/plugins/jqueryKnob/jquery.knob.js') ?>" type="text/javascript"></script>
-        <!-- daterangepicker -->
-        <script src="<?php echo base_url('assets/js/plugins/daterangepicker/daterangepicker.js') ?>" type="text/javascript"></script>
-        <!-- datepicker -->
-        <script src="<?php echo base_url('assets/js/plugins/datepicker/bootstrap-datepicker.js') ?>" type="text/javascript"></script>
-        <!-- Bootstrap WYSIHTML5 -->
-        <script src="<?php echo base_url('assets/js/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js') ?>" type="text/javascript"></script>
-        <!-- iCheck -->
-        <script src="<?php echo base_url('assets/js/plugins/iCheck/icheck.min.js') ?>" type="text/javascript"></script>
 
         <!-- AdminLTE App -->
         <script src="<?php echo base_url('assets/js/AdminLTE/app.js') ?>" type="text/javascript"></script>
