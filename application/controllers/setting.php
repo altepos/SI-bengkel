a20908a9e83196ed763ed1556016f0fbddbe9d12
haqisaurus <haqisaurus@gmai.com>
2014-12-01 22:25:28 +0700

    - update setting page

---
+++ application/controllers/setting.php
@@ -0,0 +1,20 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Setting extends CI_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+        // $this->load->model('member_model', 'member');
+    }
+
+    public function index()
+    {               
+        //SoedjarwoAli: load barang view 
+        $data['content'] = $this->load->view('pages/setting', '', true);
+        $this->load->view('master_page', $data);
+    }
+}
+
+/* End of file barang.php */
+/* Location: ./application/controllers/barang.php */
