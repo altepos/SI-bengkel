d7c63f5626e00bdefe49c8decbc230cedebea544
soedjarwoali <soedjarwo.ali@gmail.com>
2014-12-01 21:27:39 +0700

    - update side menu

--- application/views/master_page.php
+++ application/views/master_page.php
@@ -430,6 +430,74 @@
                 <!-- /.sidebar -->
             </aside>
 
+            <aside class="left-side sidebar-offcanvas lil-menu" style="width: 60px;">
+                <!-- sidebar: style can be found in sidebar.less -->
+                <section class="sidebar">
+                    <!-- Sidebar user panel -->
+                    <div class="user-panel">
+                        <div class="pull-left image">
+                            <img src="<?php echo base_url('') ?>assets/img/avatar3.png" class="img-circle" alt="User Image" />
+                        </div>
+                    </div>
+                    <!-- search form -->
+                    <form action="#" method="get" class="sidebar-form">
+                        <div class="input-group">
+                            <!-- <input type="text" name="q" class="form-control" placeholder="Search..."/> -->
+                            <span class="input-group-btn">
+                                <button type='submit' name='search' id='search-btn' class="btn btn-flat"><i class="fa fa-search"></i></button>
+                            </span>
+                        </div>
+                    </form>
+                    <!-- /.search form -->
+                    <!-- sidebar menu: : style can be found in sidebar.less -->
+                    <ul class="sidebar-menu">
+                        <li class="active">
+                            <a href="<?php echo base_url('home') ?>">
+                                <i class="fa fa-dashboard"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="<?php echo base_url('user') ?>">
+                                <i class="fa fa-user"></i>
+                            </a>
+                        </li>
+                        <li class="treeview">
+                            <a href="#">
+                                <i class="fa fa-cubes"></i>
+                                <i class="fa fa-angle-left pull-right"></i>
+                            </a>
+                            <ul class="treeview-menu">
+                                <li><a href="<?php echo base_url('barang') ?>"><i class="fa fa-list-ol"></i></a></li>
+                                <li><a href="<?php echo base_url('merk') ?>"><i class="fa fa-qrcode"></i></a></li>
+                                <li><a href="<?php echo base_url('kategori') ?>"><i class="fa fa-tags"></i></a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="<?php echo base_url('supplier') ?>">
+                                <i class="fa fa-truck"></i>
+                            </a>
+                        </li>
+                        <li class="treeview">
+                            <a href="#">
+                                <i class="fa fa-file-text"></i>
+                                <i class="fa fa-angle-left pull-right"></i>
+                            </a>
+                            <ul class="treeview-menu">
+                                <li><a href="<?php echo base_url('barang') ?>"><i class="fa fa-wrench"></i></a></li>
+                                <li><a href="<?php echo base_url('merk') ?>"><i class="fa fa-tags"></i></a></li>
+                                <li><a href="<?php echo base_url('kategori') ?>"><i class="fa fa-flag"></i></a></li>
+                            </ul>
+                        </li>
+                           <li>
+                            <a href="<?php echo base_url('setting') ?>">
+                                <i class="fa fa-cogs"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </section>
+                <!-- /.sidebar -->
+            </aside>
+
             <!-- Right side column. Contains the navbar and content of the page -->
             <aside class="right-side strech">
                 
