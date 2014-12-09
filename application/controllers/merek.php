c3d93caabed472845d89cb1e6796e6adfbb5be56
haqisaurus <haqisaurus@gmai.com>
2014-12-09 21:27:49 +0700

    + add controller

--- application/controllers/merek.php
+++ application/controllers/merek.php
@@ -1,11 +1,11 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Merk extends CI_Controller {
+class Merek extends CI_Controller {
 
     public function __construct()
     {
         parent::__construct();
-        // $this->load->model('member_model', 'member');
+        $this->load->helper('url');
     }
 
     public function index()
