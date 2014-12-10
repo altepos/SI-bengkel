957c31c6d1c2a1da7e1231538fbeb422d487a683
haqisaurus <haqisaurus@gmail.com>
2014-12-10 13:22:52 +0700

    - update fix orm

--- application/controllers/merek.php
+++ application/controllers/merek.php
@@ -5,7 +5,6 @@ class Merek extends CI_Controller {
     public function __construct()
     {
         parent::__construct();
-        $this->load->helper('url');
     }
 
     public function index()
