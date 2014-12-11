c57aecfd660af45824ddbea258f0a6d013c00e0b
haqisaurus <haqisaurus@gmai.com>
2014-12-11 19:19:59 +0700

    + add new table schema

--- _db/mydb.sql
+++ _db/mydb.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 3.2.4
+-- version 4.2.11
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 15, 2014 at 03:13 PM
--- Server version: 5.1.41
--- PHP Version: 5.3.1
+-- Generation Time: Dec 11, 2014 at 01:16 PM
+-- Server version: 5.6.21
+-- PHP Version: 5.6.3
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -16,7 +17,7 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `sibengkel`
+-- Database: `mydb`
 --
 
 -- --------------------------------------------------------
@@ -36,14 +37,8 @@ CREATE TABLE IF NOT EXISTS `barang` (
   `potongan_harga` int(11) DEFAULT NULL,
   `letak_gudang` varchar(45) DEFAULT NULL,
   `merek_id` int(11) NOT NULL,
-  `kategori_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
+  `kategori_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `barang`
---
-
 
 -- --------------------------------------------------------
 
@@ -56,14 +51,8 @@ CREATE TABLE IF NOT EXISTS `belanja` (
   `tanggal` int(11) DEFAULT NULL,
   `total` int(11) DEFAULT NULL,
   `user_id` int(11) NOT NULL,
-  `supplier_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
+  `supplier_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `belanja`
---
-
 
 -- --------------------------------------------------------
 
@@ -73,14 +62,8 @@ CREATE TABLE IF NOT EXISTS `belanja` (
 
 CREATE TABLE IF NOT EXISTS `kategori` (
   `id` int(11) NOT NULL,
-  `nama` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `nama` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `kategori`
---
-
 
 -- --------------------------------------------------------
 
@@ -90,14 +73,8 @@ CREATE TABLE IF NOT EXISTS `kategori` (
 
 CREATE TABLE IF NOT EXISTS `merek` (
   `id` int(11) NOT NULL,
-  `nama` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `nama` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `merek`
---
-
 
 -- --------------------------------------------------------
 
@@ -108,14 +85,8 @@ CREATE TABLE IF NOT EXISTS `merek` (
 CREATE TABLE IF NOT EXISTS `phone` (
   `id` int(11) NOT NULL,
   `no_telp` varchar(45) DEFAULT NULL,
-  `user_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
+  `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `phone`
---
-
 
 -- --------------------------------------------------------
 
@@ -125,14 +96,8 @@ CREATE TABLE IF NOT EXISTS `phone` (
 
 CREATE TABLE IF NOT EXISTS `role` (
   `id` int(11) NOT NULL,
-  `role` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `role` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `role`
---
-
 
 -- --------------------------------------------------------
 
@@ -142,14 +107,8 @@ CREATE TABLE IF NOT EXISTS `role` (
 
 CREATE TABLE IF NOT EXISTS `status` (
   `id` int(11) NOT NULL,
-  `status` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `status` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `status`
---
-
 
 -- --------------------------------------------------------
 
@@ -161,14 +120,8 @@ CREATE TABLE IF NOT EXISTS `supplier` (
   `id` int(11) NOT NULL,
   `nama` varchar(200) DEFAULT NULL,
   `alamat` text,
-  `no_telp` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `no_telp` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `supplier`
---
-
 
 -- --------------------------------------------------------
 
@@ -182,14 +135,8 @@ CREATE TABLE IF NOT EXISTS `transaksi` (
   `cash_back` int(11) DEFAULT NULL,
   `tanggal` varchar(45) DEFAULT NULL,
   `user_id` int(11) NOT NULL,
-  `status_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
+  `status_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `transaksi`
---
-
 
 -- --------------------------------------------------------
 
@@ -202,14 +149,8 @@ CREATE TABLE IF NOT EXISTS `transaksi_detail` (
   `harga_terjual` int(11) DEFAULT NULL,
   `jumlah_terjual` int(11) DEFAULT NULL,
   `transaksi_id` int(11) NOT NULL,
-  `belanja_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
+  `belanja_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `transaksi_detail`
---
-
 
 -- --------------------------------------------------------
 
@@ -228,14 +169,78 @@ CREATE TABLE IF NOT EXISTS `user` (
   `no_identitas` varchar(45) DEFAULT NULL,
   `terakhir_login` datetime DEFAULT NULL,
   `tanggal_dibuat` varchar(45) DEFAULT NULL,
-  `role_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
+  `role_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `user`
+-- Indexes for dumped tables
 --
 
+--
+-- Indexes for table `barang`
+--
+ALTER TABLE `barang`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `belanja`
+--
+ALTER TABLE `belanja`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `kategori`
+--
+ALTER TABLE `kategori`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `merek`
+--
+ALTER TABLE `merek`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `phone`
+--
+ALTER TABLE `phone`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `role`
+--
+ALTER TABLE `role`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `status`
+--
+ALTER TABLE `status`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `supplier`
+--
+ALTER TABLE `supplier`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `transaksi_detail`
+--
+ALTER TABLE `transaksi_detail`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+ ADD PRIMARY KEY (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
